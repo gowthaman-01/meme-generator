d15682de19d714198a42d701e1351fc7799504fa
Gowthaman Aravindan <gowthaman169@gmail.com>
2022-04-05 06:27:33 +0800

    Added generate image functionaity

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,28 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import memeData from "../data/memeData"
 
 const Meme = () => {
+    const imageSource = memeData.data.memes;
+    const [meme, setMeme] = useState("");
+    const showMeme = () => {
+        const randomNumber = Math.floor(Math.random() * imageSource.length)
+        const imageUrl = imageSource[randomNumber].url
+        setMeme(imageUrl)
+    }
+
     return (
         <div>
-            <form className="meme">
-                <div className="form">
-                    <input
-                        type="text"
-                        placeholder="Top text"
-                        className="form-input"
-                        id="left-input"
-                    />
-                    <input
-                        type="text"
-                        placeholder="Bottom text"
-                        className="form-input"
-                        id="right-input"
-                    />
-                </div>
-                <div className="button">
-                    <button className="form-button" >Generate image</button>
-                </div>
-
-            </form>
+            <div className="form">
+                <input
+                    type="text"
+                    placeholder="Top text"
+                    className="form-input"
+                    id="left-input"
+                />
+                <input
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form-input"
+                    id="right-input"
+                />
+            </div>
+            <div className="button">
+                <button className="form-button" onClick={showMeme}>Generate image</button>
+            </div>
+            <div className="meme-template-container">
+                <img src={meme} className="meme-template" alt="meme" />
+            </div>
         </div>
 
     )
